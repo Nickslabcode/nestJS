4f8c9ab205f7325b6e3ca8301c5310d7b3c8bda7
Nikola Nenovski <nicknenovski@gmail.com>
2024-10-23 15:29:27 +0300

    learning how to inject the request body in a service

--- src/common/pagination/providers/pagination.service.ts
+++ src/common/pagination/providers/pagination.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { PaginationQueryDto } from '../dtos/pagination-query.dto';
 import { ObjectLiteral, Repository } from 'typeorm';
 
+import { REQUEST } from '@nestjs/core';
+import { Request } from '@nestjs/common';
+
 @Injectable()
 export class PaginationService {
+  constructor(
+    /**
+     * Injecting Request
+     */
+    @Inject(REQUEST)
+    private readonly request: Request,
+  ) {}
+
   public async paginateQuery<T extends ObjectLiteral>(
     paginationQuery: PaginationQueryDto,
     repository: Repository<T>,
