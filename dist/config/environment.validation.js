6b17ec55f5b9ef4b2cab7a094642249b275ab9d6
Nikola Nenovski <nicknenovski@gmail.com>
2024-10-25 14:16:32 +0300

    JWT environment validation

--- dist/config/environment.validation.js
+++ dist/config/environment.validation.js
@@ -11,5 +11,9 @@ exports.default = Joi.object({
     DATABASE_NAME: Joi.string().required(),
     DATABASE_USER: Joi.string().required(),
     PROFILE_API_KEY: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
+    JWT_TOKEN_AUDIENCE: Joi.string().required(),
+    JWT_TOKEN_ISSUER: Joi.string().required(),
+    JWT_ACCESS_TOKEN_TTL: Joi.number().default(3600),
 });
 //# sourceMappingURL=environment.validation.js.map
