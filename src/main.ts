5c89a375910f45e98bea962bf11ac841a2799cee
Nikola Nenovski <nicknenovski@gmail.com>
2024-10-28 13:27:43 +0200

    configuring aws s3 bucket for file uploads

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,7 +22,7 @@ async function bootstrap() {
   /**
    * Swagger configuration
    */
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('NestJS MasterClass - Blog app API')
     .setDescription('Use the base API URL as http://localhost:3000')
     .setVersion('1.0')
@@ -29,8 +31,18 @@ async function bootstrap() {
     .addServer('http://localhost:3000')
     .build();
   // Instantiate document
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
+
+  // Set up AWS SDK for uploading files to AWS S3 bucket
+  const configService = app.get(ConfigService); // makes the env variables available to main.ts
+  config.update({
+    credentials: {
+      accessKeyId: configService.get('appConfig.awsAccessKeyId'),
+      secretAccessKey: configService.get('appConfig.awsSecretAccessKey'),
+    },
+    region: configService.get('appConfig.awsRegion'),
+  });
 
   // Enable cors
   app.enableCors();
