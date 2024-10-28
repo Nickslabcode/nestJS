16a2cd6fe948a037496d50a748cfb8a363727843
Nikola Nenovski <nicknenovski@gmail.com>
2024-10-28 14:41:24 +0200

    UploadToAwsService implementation, created a generalFileName method for unique file names with uuid

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './providers/uploads.service';
+import { UploadToAwsService } from './providers/upload-to-aws.service';
 
 @Module({
   controllers: [UploadsController],
-  providers: [UploadsService]
+  providers: [UploadsService, UploadToAwsService]
 })
 export class UploadsModule {}
