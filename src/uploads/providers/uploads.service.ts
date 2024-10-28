468ca90f1e30370c36d1d7789f97a3c4e58497f8
Nikola Nenovski <nicknenovski@gmail.com>
2024-10-28 13:52:17 +0200

    Created uploads controller with correct interceptor and swagger decorators. Injected uploadsService

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UploadsService {}
+export class UploadsService {
+  public async uploadFile(file: Express.Multer.File) {
+    // Upload the file to AWS S3 to get the url
+    // Generate a new entry in db
+  }
+}
