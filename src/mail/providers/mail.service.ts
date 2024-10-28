047d95e0e6c8c2b4dde8faa8538b15da20b5a264
Nikola Nenovski <nicknenovski@gmail.com>
2024-10-28 16:40:29 +0200

    MailService implementation. Created sendUserWelcome method

--- src/mail/providers/mail.service.ts
+++ src/mail/providers/mail.service.ts
@@ -1,4 +1,27 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(
+    /**
+     * Injecting mailerService
+     */
+    private readonly mailerService: MailerService,
+  ) {}
+
+  public async sendUserWelcome(user: User): Promise<void> {
+    await this.mailerService.sendMail({
+      to: user.email,
+      from: `Onboarding Team <support@nestjs-blog.com>`, // setting the from email manually
+      subject: 'Welcome to NestJS blog',
+      template: './welcome',
+      context: {
+        name: user.firstName,
+        email: user.email,
+        loginUrl: 'http://localhost:3000',
+      },
+    });
+  }
+}
